723ad1d8f0f193e6665243fd6bd44c060ef2910a
AibarBaltykhan <52800030+AibarBaltykhan@users.noreply.github.com>
2020-10-01 23:22:58 +0600

    init: index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == null) {
+         return 0;
+    }
+    if (array.length > 0) {
+        let minNum = array[0]
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] < minNum) {
+                minNum = array[i];
+            }
+        }
+        return minNum
+    } else { 
+        return 0;
+    }    
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == null) {
+        return 0;
+   }
+    if (array.length > 0) {
+        let maxNum = array[0]
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] > maxNum) {
+                maxNum = array[i];
+            }
+        }
+        return maxNum
+    } else { 
+        return 0;
+    }    
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == null) {
+        return 0;
+   }
+    if (array.length > 0) {
+        let sum = 0
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        let avgNum = sum/array.length; 
+        return avgNum;
+    } else { 
+        return 0;
+    }    
 }
+
